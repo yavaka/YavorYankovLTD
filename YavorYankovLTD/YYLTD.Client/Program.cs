1f40ed1122b70ee6f32b4ee8866cfe5ba27c26db
yavaka <yavakaa@gmail.co m>
2018-05-12 18:39:39 +0100

    Add client

---
+++ YavorYankovLTD/YYLTD.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YYLTD.Data;
+
+namespace YYLTD.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new YYLTDDbContext)
+            {
+                db.Database.Initialize(true);
+            }
+        }
+    }
+}
