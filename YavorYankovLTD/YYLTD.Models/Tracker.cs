490d074319b9525fa7073dc0af521a90cd235cca
yavaka <yavakaa@gmail.co m>
2018-05-12 18:25:20 +0100

    Add Models (Tracker)

---
+++ YavorYankovLTD/YYLTD.Models/Tracker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Tracker
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public short Route { get; set; }
+
+        public short Miles { get; set; }
+
+        public DateTime StartIn { get; set; }
+
+        public DateTime EndIn { get; set; }
+
+        public short StopsToDeliver { get; set; }
+
+        public bool IsWork { get; set; }
+    }
+}
